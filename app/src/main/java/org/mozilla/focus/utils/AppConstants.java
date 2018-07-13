b63afa5fdfcde7643ef0415e4352c2ec778e642e
Stefan Arentz <stefan@arentz.ca>
2018-07-13 16:18:40 -0400

    Fixes #2904 - Coverage builds submit telemetry

--- app/src/main/java/org/mozilla/focus/utils/AppConstants.java
+++ app/src/main/java/org/mozilla/focus/utils/AppConstants.java
@@ -16,16 +16,16 @@ public final class AppConstants {
 
     private AppConstants() {}
 
-    public static boolean isDevBuild() {
-        return BUILD_TYPE_DEBUG.equals(BuildConfig.BUILD_TYPE);
-    }
-
     public static boolean isKlarBuild() {
         return PRODUCT_FLAVOR_KLAR.equals(BuildConfig.FLAVOR_product);
     }
 
     public static boolean isReleaseBuild() {
         return BUILD_TYPE_RELEASE.equals(BuildConfig.BUILD_TYPE);
+    }
+
+    public static boolean isDevBuild() {
+        return !isReleaseBuild();
     }
 
     public static boolean isGeckoBuild() {
